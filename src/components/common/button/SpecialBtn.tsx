6fc3849f5105b5e146a6a29311cfb41be99f440f
박재서 <wotjoo@naver.com>
2026-06-02 09:32:23 +0900

    Deploy portfolio site

--- src/components/common/button/SpecialBtn.tsx
+++
@@ -1,7 +0,0 @@
-import { ComponentProps } from "react";
-
-type Props = ComponentProps<"button">;
-
-export default function SpecialBtn({ className, ...props }: Props) {
-  return <button className={`specialBtn ${className}`} {...props} />;
-}
--- src/constant/sectionId.ts
+++
@@ -1,8 +0,0 @@
-export const SectionID = {
-	aboutMe: 'about-me',
-	skills: 'skills',
-	mainProjects: 'main-projects',
-	subProjects: 'sub-projects',
-	contact: 'contact',
-	mainContent: 'main-content',
-};
